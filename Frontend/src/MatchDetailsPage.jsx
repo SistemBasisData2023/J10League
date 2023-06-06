868ee365a7f994d4b14978a3e671d2ab3ef3eb62
fathanmuhandis <m.fathan11@ui.ac.id>
2023-06-07 02:25:13 +0700

    fix match details pages

--- Frontend/src/MatchDetailsPage.jsx
+++ Frontend/src/MatchDetailsPage.jsx
@@ -5,19 +5,20 @@ const LoginPage = () => {
     return (
         <div className="bg-primary w-full relative pt-9">
             <Navbar />
-            <div className={`bg-primary${style.flexStart} w-full h-screen flex flex-row justify-center items-center gap-24`}>
+
+            <div className={`bg-primary ${style.flexStart}`}>
                 <div className={`${style.boxWidth}`}>
                     <MatchDetails />
                 </div>
             </div>
 
-            <div className={`bg-primary ${style.paddingX} ${style.flexCenter} flex-col md:absolute md:bottom-0 w-screen`}>
+            <div className={`bg-primary ${style.paddingX} ${style.flexCenter}`}>
                 <div className={`${style.boxWidth}`}>
                     <Footer />
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 export default LoginPage;
