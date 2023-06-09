2c1cd71c959f6018b4d9db8922279e0468cc7e22
Jeffri <jeffri11@ui.ac.id>
2023-06-09 18:06:48 +0700

    Updated and tested some insert requests

--- Backend/server.js
+++ Backend/server.js
@@ -109,14 +109,16 @@ app.post("/LoginAdmin", async (req, res) => {
 // ini masih kasar, gak tau bener apa nggak, jangan uncomment dulu -jep
 
 app.post('/InsertTeam', (req, res) => {
+  const code = req.body.code
+  const name = req.body.name
   db.query(
-    `INSERT INTO teams (team_code, team_name) VALUES ('${req.body.code}', '${req.body.name}');`,
+    `INSERT INTO teams (team_code, team_name) VALUES ('${code}', '${name}');`,
     (err, result) => {
       if (err) {
         console.error("Error executing query", err);
         return
       }
-      res.json(result.rows) //post will return the inserted rows as json, dunno if will work or not, and kinda unnecessary so just erase if not needed
+      //res.json(result.rows) //post will return the inserted rows as json, dunno if will work or not, and kinda unnecessary so just erase if not needed
     }
   );
 });
@@ -125,9 +127,9 @@ app.post('/InsertTeam', (req, res) => {
 //atau mending team_id ganti team_name, soalnya pas masukin member user taunya nama tim, bukan id tim kan?
 app.post('/InsertTeamInfo', (req, res) => {
   const team_id = req.body.team_id
-  var team_code
-  var member_count
-  var member_id
+  let team_code;
+  let member_count;
+  let member_id;
 
   //first, get the team_code from teams table, needed for member_id generation
   const getTeamQuery = `SELECT * FROM teams WHERE team_id = ${team_id};`
@@ -139,7 +141,7 @@ app.post('/InsertTeamInfo', (req, res) => {
       console.error("Error executing query", err);
       return
     }
-    team_code = result.body.team_code
+    team_code = result.rows[0].team_code;
   });
 
   db.query(memberCountQuery, (err, result) => {
@@ -147,11 +149,9 @@ app.post('/InsertTeamInfo', (req, res) => {
       console.error("Error executing query", err);
       return
     }
-    member_count = result.body.COUNT(member_id)
-    //COUNT(member_id) is supposed to be the column name where the count number is stored, but dunno how correct case-sensitive? might have to test and revise
+    member_count = result.rows[0].count;
+    member_id = team_code + "_" + (++member_count) //auto-generate member_id from team_code, concatenate with underscore(_) and (member_count + 1)
   });
-
-  member_id = team_code + "_" + (++member_count) //auto-generate member_id from team_code, concatenate with underscore(_) and (member_count + 1)
 
   //finally use all the available resource to create new record in team_info table
   const finalQuery = `INSERT INTO team_info VALUES (${team_id}, '${member_id}', '${req.body.member_name}', '${req.body.member_role}');`
@@ -161,7 +161,7 @@ app.post('/InsertTeamInfo', (req, res) => {
       console.error("Error executing query", err);
       return
     }
-    res.json(result.rows) //post will return the inserted rows as json, dunno if will work or not, and kinda unnecessary so just erase if not needed
+    //res.json(result.rows) //post will return the inserted rows as json, dunno if will work or not, and kinda unnecessary so just erase if not needed
   });
 });
 
