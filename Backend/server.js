b167838660e3ba8d70b9de1b3c3bb32a2bd7f652
Jeffri <jeffri11@ui.ac.id>
2023-06-10 23:15:04 +0700

    Revision on /loginAdmin and added more functions

--- Backend/server.js
+++ Backend/server.js
@@ -76,13 +76,13 @@ app.post("/RegisterAdmin", async (req, res) => {
 });
 
 app.post("/LoginAdmin", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, pass } = req.body;
   
   db.query(
     `SELECT * FROM admin WHERE username = '${username}'`,
     async (err, result) => {
       if (result.rows.length === 0) {
-        alert("Invalid username or password");
+        window.alert("Username not found");
         return res.status(401).json({ error: "Invalid username or password" });
       }
       if (err) {
@@ -90,12 +90,12 @@ app.post("/LoginAdmin", async (req, res) => {
         return;
       }
       const storedData = result.rows[0];
-      bcrypt.compare(password, storedData.password_hash, (err, isMatch) => {
+      bcrypt.compare(pass, storedData.pass, (err, isMatch) => {
         if (err) {
           console.error("Error comparing password", err);
           return;
         }
-        if (isMatch) {
+        if (!isMatch) {
           return res.status(200).json({ message: "Login success" });
         } else {
           return res.status(401).json({ error: "Invalid username or password" });
@@ -131,7 +131,7 @@ app.post('/InsertTeamInfo', (req, res) => {
   //first, get the team_code from teams table, needed for member_id generation
   const getTeamQuery = `SELECT * FROM teams WHERE team_name = ${team_name};`
   //next, count the existing member in a team, to decide the number on member_id
-  const memberCountQuery = `SELECT COUNT(member_id) FROM team_info WHERE team_code = ${team_code};`
+  const memberCountQuery = `SELECT COUNT(member_code) FROM team_info WHERE team_code = ${team_code};`
 
   db.query(getTeamQuery, (err, result) => {
     if (err) {
@@ -152,7 +152,7 @@ app.post('/InsertTeamInfo', (req, res) => {
   });
 
   //finally use all the available resource to create new record in team_info table
-  const finalQuery = `INSERT INTO team_info VALUES (${team_code}, '${member_code}', '${req.body.member_name}', '${req.body.member_role}');`
+  const finalQuery = `INSERT INTO team_info VALUES ('${team_code}', '${member_code}', '${req.body.member_name}', '${req.body.member_role}');`
 
   db.query(finalQuery, (err, result) => {
     if (err) {
@@ -166,17 +166,21 @@ app.post('/InsertTeamInfo', (req, res) => {
 app.post('/InsertTournament', (req, res) => {
   const code = req.body.code
   const name = req.body.name
-  var status
+  let status = ""
   const start_date = req.body.start_date
   const end_date = req.body.end_date
   const current_date = new Date()
   
   //function buat check tanggal sekarang sama start date end date, buat nentuin status
   if(current_date < Date(String(start_date))){
-
+    status = "Upcoming"
+  } else if(current_date > Date(String(end_date))){
+    status = "Completed"
+  } else {
+    status = "Ongoing"
   }
 
-  const query = `INSERT INTO tournaments (tournament_code, tournament_name, tournament_status, start_date, end_date) VALUES ('${code}', '${name}', '${status}', '${start_date}', '${end_date}');`
+  const query = `INSERT INTO tournaments VALUES ('${code}', '${name}', '${status}', '${start_date}', '${end_date}');`
 
   db.query(query, (err, result) => {
     if (err) {
@@ -188,7 +192,98 @@ app.post('/InsertTournament', (req, res) => {
 });
 
 app.post('/InsertMatch', (req, res) => {
-  
+  let match_code = ""
+  const tournament_name = req.body.tournament_name
+  const team_1_code = req.body.team_1_code
+  const team_2_code = req.body.team_2_code
+  const match_date = req.body.match_date
+  let status = ""
+  const stage = req.body.stage
+  const round_count = req.body.round_count
+  let tournament_code = ""
+  let match_count = 0
+  let round_code = []
+  const column_name = ["first_round_code", "second_round_code", "third_round_code",
+    "fourth_round_code", "fifth_round_code", "sixth_round_code", "seventh_round_code"]
+
+  //first, get the tournament_code from tournaments table, needed for match_code generation
+  const getTournamentQuery = `SELECT * FROM tournaments WHERE tournament_name = ${tournament_name};`
+  //next, count the existing matches in a tournament, to decide the number on match_code
+  const matchCountQuery = `SELECT COUNT(match_code) FROM match_info WHERE tournament_code = ${tournament_code};`
+
+  db.query(getTournamentQuery, (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+      return
+    }
+    tournament_code = result.rows[0].tournament_code;
+  });
+
+  db.query(matchCountQuery, (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+      return
+    }
+    match_count = result.rows[0].count;
+    //auto-generate match_code from tournament_code, concatenate with underscore(_) and (match_count + 1)
+    match_code = tournament_code + "_" + (++match_count)
+  });
+
+  switch(round_count){
+    case 3:
+      for(let i = 3; i > 0; i--){
+        round_code[i-1] = match_code + "_" + i
+      }
+      const roundQuery3 = `UPDATE match_info
+                          SET ${column_name[0]} = '${round_code[0]}'
+                              ${column_name[1]} = '${round_code[1]}'
+                              ${column_name[2]} = '${round_code[2]}'
+                          WHERE match_code = '${match_code}';`
+      
+      break
+    case 5:
+      for(let i = 5; i > 0; i--){
+        round_code[i-1] = match_code + "_" + i
+      }
+      const roundQuery5 = `UPDATE match_info
+                          SET ${column_name[0]} = '${round_code[0]}'
+                              ${column_name[1]} = '${round_code[1]}'
+                              ${column_name[2]} = '${round_code[2]}'
+                              ${column_name[3]} = '${round_code[3]}'
+                              ${column_name[4]} = '${round_code[4]}'
+                          WHERE match_code = '${match_code}';`
+      
+      break
+    case 7:
+      for(let i = 7; i > 0; i--){
+        round_code[i-1] = match_code + "_" + i
+      }
+      const roundQuery7 = `UPDATE match_info
+                          SET ${column_name[0]} = '${round_code[0]}'
+                              ${column_name[1]} = '${round_code[1]}'
+                              ${column_name[2]} = '${round_code[2]}'
+                              ${column_name[3]} = '${round_code[3]}'
+                              ${column_name[4]} = '${round_code[4]}'
+                              ${column_name[5]} = '${round_code[5]}'
+                              ${column_name[6]} = '${round_code[6]}'
+                          WHERE match_code = '${match_code}';`
+      
+      break
+    default:
+      console.error("Invalid round count");
+      return
+  }
+
+  //finally use all the available resource to create new record in team_info table
+  const finalQuery = `INSERT INTO match_info VALUES ('${match_code}', '${tournament_code}', '${team_1_code}', '${team_2_code}', '${match_date}');`
+
+  db.query(finalQuery, (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+      return
+    }
+    res.send("Team member data inserted successfully.")
+  });
 });
 
 //
