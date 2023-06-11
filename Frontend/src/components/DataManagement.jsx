509d9f22b6c76e97bc6d8424993ac28a5737d5f1
fathanmuhandis <m.fathan11@ui.ac.id>
2023-06-11 23:29:54 +0700

    coba fitur update di data management

--- Frontend/src/components/DataManagement.jsx
+++ Frontend/src/components/DataManagement.jsx
@@ -5,15 +5,19 @@ import axios from "axios";
 
 const Upcoming = ({ props, index }) => {
     const [matches, setMatches] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [team_1_code, setTeam_1_code] = useState("");
+    const [team_2_code, setTeam_2_code] = useState("");
+    const [match_date, setMatch_date] = useState("");
 
     const formatMatchDate = (dateString) => {
         const date = new Date(dateString);
         const day = date.getDate().toString().padStart(2, '0');
         const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
         const year = date.getFullYear().toString();
-    
+
         return `${day}-${month}-${year}`;
-      };
+    };
 
     useEffect(() => {
         fetch("http://localhost:3001/upcomingMatches")
@@ -35,6 +39,28 @@ const Upcoming = ({ props, index }) => {
             })
             .catch((error) => {
                 console.error("Error deleting match", error);
+            });
+    };
+
+    //Make put request of handleUpdate function that fetches the data from "http://localhost:3001/updateUpcomingMatch" and updates the data in the database
+    const handleUpdate = (match_code) => {
+        axios
+            .put(`http://localhost:3001/updateUpcomingMatch/${match_code}`, {
+                team_1_code: team_1_code,
+                team_2_code: team_2_code,
+                match_date: match_date,
+                // match_date: moment(match_date).format('DD-MM-YYYY'),
+            })
+            .then((response) => {
+                console.log("Match updated successfully");
+                // Fetch updated data after update
+                fetch("http://localhost:3001/upcomingMatches")
+                    .then((response) => response.json())
+                    .then((data) => setMatches(data))
+                    .catch((error) => console.error("Error fetching data", error));
+            })
+            .catch((error) => {
+                console.error("Error updating match", error);
             });
     };
 
@@ -79,9 +105,65 @@ const Upcoming = ({ props, index }) => {
                                     </td>
                                     <td> {formatMatchDate(match.match_date)} </td>
                                     <td>
-                                        <button className="btn btn-ghost w-14 h-14">
+                                        <button onClick={() => setShowModal(true)} className="btn btn-ghost w-14 h-14 z-50">
                                             <img src={assets['edit']} />
                                         </button>
+                                        {showModal ? (
+                                            <>
+                                                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                                        {/*content*/}
+                                                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-primary outline-none focus:outline-none">
+                                                            {/*header*/}
+                                                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                                                <h3 className="text-3xl font-semibold">
+                                                                    EDIT DATA
+                                                                </h3>
+                                                                <button
+                                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                                    onClick={() => setShowModal(false)}
+                                                                >
+                                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                                        ×
+                                                                    </span>
+                                                                </button>
+                                                            </div>
+                                                            {/*body*/}
+                                                            <div className="relative p-6 flex-auto">
+                                                                <form onSubmit={handleUpdate}>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="team_1_code" className="block mb-2">Team 1</label>
+                                                                        <input type="text" id="team_1_code" name="team_1_code" className="w-full px-3 py-2 border rounded-md" value={team_1_code} onChange={(event) => setTeam_1_code(event.target.value)} required />
+                                                                    </div>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="team_2_code" className="block mb-2">Team 2</label>
+                                                                        <input type="text" id="team_2_code" name="team_2_code" className="w-full px-3 py-2 border rounded-md" value={team_2_code} onChange={(event) => setTeam_2_code(event.target.value)} required />
+                                                                    </div>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="match_date" className="block mb-2">Match Date</label>
+                                                                        <input type="date" id="match_date" name="match_date" className="w-full px-3 py-2 border rounded-md" value={match_date} onChange={(event) => setMatch_date(event.target.value)} required />
+                                                                    </div>
+                                                                    <button type="submit" className="gap-x-10 font-poppins font-semibold text-[18px] text-primary bg-blue-gradient px-4 py-2 rounded-md mt-2" onClick={() => handleUpdate(match.match_code)}>Save Changes</button>
+                                                                </form>
+                                                            </div>
+                                                            {/*footer*/}
+                                                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                                                <button
+                                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                                    type="button"
+                                                                    onClick={() => setShowModal(false)}
+                                                                >
+                                                                    Close
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                            </>
+                                        ) : null}
+
+
                                         <button className="btn btn-ghost w-14 h-14" onClick={() => handleDelete(match.match_code)}>
                                             <img src={assets['trash']} />
                                         </button>
@@ -98,6 +180,10 @@ const Upcoming = ({ props, index }) => {
 
 const Results = ({ props, index }) => {
     const [matches, setMatches] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [match_winner, setMatch_winner] = useState("");
+    const [team_1_score, setTeam_1_score] = useState("");
+    const [team_2_score, setTeam_2_score] = useState("");
 
     useEffect(() => {
         fetch("http://localhost:3001/resultsMatches")
@@ -119,6 +205,27 @@ const Results = ({ props, index }) => {
             })
             .catch((error) => {
                 console.error("Error deleting match", error);
+            });
+    };
+
+    const handleUpdate = (match_code) => {
+        axios
+            .put(`http://localhost:3001/updateUpcomingMatch/${match_code}`, {
+                team_1_code: team_1_code,
+                team_2_code: team_2_code,
+                match_date: match_date,
+                // match_date: moment(match_date).format('DD-MM-YYYY'),
+            })
+            .then((response) => {
+                console.log("Match updated successfully");
+                // Fetch updated data after update
+                fetch("http://localhost:3001/upcomingMatches")
+                    .then((response) => response.json())
+                    .then((data) => setMatches(data))
+                    .catch((error) => console.error("Error fetching data", error));
+            })
+            .catch((error) => {
+                console.error("Error updating match", error);
             });
     };
 
@@ -162,9 +269,63 @@ const Results = ({ props, index }) => {
                                     </td>
                                     <td> {match.team_1_score} - {match.team_2_score} </td>
                                     <td>
-                                        <button className="btn btn-ghost w-14 h-14">
+                                        <button onClick={() => setShowModal(true)} className="btn btn-ghost w-14 h-14 z-50">
                                             <img src={assets['edit']} />
                                         </button>
+                                        {showModal ? (
+                                            <>
+                                                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                                        {/*content*/}
+                                                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-primary outline-none focus:outline-none">
+                                                            {/*header*/}
+                                                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                                                <h3 className="text-3xl font-semibold">
+                                                                    EDIT DATA
+                                                                </h3>
+                                                                <button
+                                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                                    onClick={() => setShowModal(false)}
+                                                                >
+                                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                                        ×
+                                                                    </span>
+                                                                </button>
+                                                            </div>
+                                                            {/*body*/}
+                                                            <div className="relative p-6 flex-auto">
+                                                                <form onSubmit={handleUpdate}>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="team_1_code" className="block mb-2">Match Winner</label>
+                                                                        <input type="text" id="team_1_code" name="team_1_code" className="w-full px-3 py-2 border rounded-md" value={match_winner} onChange={(event) => setMatch_winner(event.target.value)} required />
+                                                                    </div>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="team_2_code" className="block mb-2">Team 1 Score</label>
+                                                                        <input type="text" id="team_2_code" name="team_2_code" className="w-full px-3 py-2 border rounded-md" value={team_1_score} onChange={(event) => setTeam_1_score(event.target.value)} required />
+                                                                    </div>
+                                                                    <div className="font-poppins font-semibold text-white text-[15px] leading-[23.4px] mb-1">
+                                                                        <label htmlFor="team_2_code" className="block mb-2">Team 2 Score</label>
+                                                                        <input type="text" id="team_2_code" name="team_2_code" className="w-full px-3 py-2 border rounded-md" value={team_2_score} onChange={(event) => setTeam_2_score(event.target.value)} required />
+                                                                    </div>
+                                                                    <button type="submit" className="gap-x-10 font-poppins font-semibold text-[18px] text-primary bg-blue-gradient px-4 py-2 rounded-md mt-2" onClick={() => handleUpdate(match.match_code)}>Save Changes</button>
+                                                                </form>
+                                                            </div>
+                                                            {/*footer*/}
+                                                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                                                <button
+                                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                                    type="button"
+                                                                    onClick={() => setShowModal(false)}
+                                                                >
+                                                                    Close
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                            </>
+                                        ) : null}
                                         <button className="btn btn-ghost w-14 h-14" onClick={() => handleDeleteResult(match.match_code)}>
                                             <img src={assets['trash']} />
                                         </button>
